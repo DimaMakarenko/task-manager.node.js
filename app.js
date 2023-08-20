7a6470675f1f57f4a2db99948c4afda9754a054b
DimaMakarenko <makarenko.d.viri@gmail.com>
2023-08-20 14:05:58 +0300

    init connection to mongoDB

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect');
 
 
 // middleware
 app.use(express.json())
 
 // routes
-app.get('/hello', (req, res) => {
-  res.send('Task manager App')
-});
-
 app.use('/api/v1/tasks', tasks);
 
 const port = 3001;
 
-app.listen(port, console.log(`Server listening on port ${port}`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server listening on port ${port}`));
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start();
